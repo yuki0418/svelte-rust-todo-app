3154dd9791d91377111ea44918715ef546f05e4e
yuki0418 <keryuuki0418@gmail.com>
2024-08-14 08:49:05 +0900

    Create axum web server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,13 @@
+use axum::{routing::get, Router};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!!!");
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .expect("failed to bind to address");
+    axum::serve(listener, app).await.expect("server failed");
 }
