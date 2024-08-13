ba08f76a7612341e6949a22a2c011187b0d0763c
yuki0418 <keryuuki0418@gmail.com>
2024-08-14 08:17:59 +0900

    Add git ignore

--- backend/target/debug/backend.d
+++
@@ -1,1 +0,0 @@
-/Users/yuki/ghq/github.com/yuki0418/svelte-rust-todo-app/backend/target/debug/backend: /Users/yuki/ghq/github.com/yuki0418/svelte-rust-todo-app/backend/src/main.rs
--- backend/target/debug/deps/backend-12ae8e978705b0f6.d
+++
@@ -1,5 +0,0 @@
-/Users/yuki/ghq/github.com/yuki0418/svelte-rust-todo-app/backend/target/debug/deps/backend-12ae8e978705b0f6: src/main.rs
-
-/Users/yuki/ghq/github.com/yuki0418/svelte-rust-todo-app/backend/target/debug/deps/backend-12ae8e978705b0f6.d: src/main.rs
-
-src/main.rs:
